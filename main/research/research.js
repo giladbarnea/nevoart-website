43df9dcd55a4624c71f9c1b91239f84331c161cc
Maurann Stein <maurann@fb.com>
2020-01-18 14:19:45 +0200

    research page has pageIntro and handles artices with no images

--- main/research/research.js
+++ main/research/research.js
@@ -2,49 +2,62 @@ const ResearchPage = () => {
     async function init(selectedIndex) {
         console.log('ResearchPage init, selectedIndex: ', selectedIndex);
         const data = await fetchDict('main/research/research.json');
+        const pageIntro = data['page-intro'];
+        delete data['page-intro'];
         const articles = [];
         let emptied = false;
-        for (let [i, [title, { image, text, circle, caption }]] of enumerate(data.items())) {
-            let imgElem;
-            let cachedImage = CacheDiv[`research.${image}`];
-            if (cachedImage !== undefined) {
-                imgElem = cachedImage.removeAttr('hidden');
-            }
-            else {
-                let src;
-                if (image.includes('http') || image.includes('www')) {
-                    src = image;
+        for (let [i, [title, { image, text, caption }]] of enumerate(data.items())) {
+            let imgElem = undefined;
+            if (image) {
+                let cachedImage = CacheDiv[`research.${image}`];
+                if (cachedImage !== undefined) {
+                    imgElem = cachedImage.removeAttr('hidden');
                 }
                 else {
-                    src = `main/research/${image}`;
+                    let src;
+                    if (image.includes('http') || image.includes('www')) {
+                        src = image;
+                    }
+                    else {
+                        src = `main/research/${image}`;
+                    }
+                    imgElem = img({ src });
                 }
-                imgElem = img({ src });
+                imgElem.class('two-thirds');
             }
-            imgElem.class('two-thirds');
-            let article = elem({ tag: 'section', cls: 'main-cls'})
+            let article = elem({ tag: 'section', cls: 'main-cls' })
                 .cacheAppend({
-                title: elem({ tag: 'h2', cls: 'first-third', text: title }),
-                text: paragraph({ cls: "text, two-thirds" }).html(text),
-                img: imgElem,
-                caption: elem({tag: 'h6', cls: 'two-thirds', text: caption})
-            });
+                    title: elem({ tag: 'h2', cls: 'first-third', text: title }),
+                    text: paragraph({ cls: "text two-thirds" }).html(text),
+                    caption: elem({ tag: 'h6', cls: 'two-thirds', text: caption })
+                });
+            if (imgElem){
+                article.cacheAppend({img: imgElem})
+            }
             articles.push(article);
             if (!emptied) {
                 Home.empty();
                 emptied = true;
             }
-            Home.class('research-page').append(article);
+
         }
+        Home.class('research-page')
+            .append(
+                elem({ tag: 'section', cls: 'main-cls page-intro' }).append(
+                    elem({ tag: 'h1', cls: 'page-title seven-first', text: 'Research' }),
+                    elem({ tag: 'p', cls: 'seven-first', text: pageIntro })
+                ),
+                ...articles
+            );
         if (selectedIndex !== undefined) {
             const selectedArticle = articles[selectedIndex];
             const howFar = selectedIndex / articles.length;
-            selectedArticle.e.scrollIntoView({ behavior: "smooth",  });
+            selectedArticle.e.scrollIntoView({ behavior: "smooth", });
             await wait(howFar * 1000);
             selectedArticle.title.addClass('highlighted');
             await wait(600);
             selectedArticle.title.removeClass('highlighted');
         }
-
     }
     return { init };
 };
